1533ce9f5a7e0c7d8f64f983e46c6c97dece9d2f
Jhéssica Frois <jhessfsantos@gmail.com>
2025-12-28 06:09:26 -0300

    adicionando desafio

---
+++ desafio-classificador.js
@@ -0,0 +1,28 @@
+// VARIÁVEIS
+let nomeHeroi = "Capitão América"
+let xpHeroi = 1500
+let nivelHeroi = 5
+
+// ESTRUTURAS DE DECISÃO
+if (xpHeroi < 1000) {
+  nivel = "Ferro"
+} else if (xpHeroi >= 1001 && xpHeroi < 2000) {
+  nivel = "Bronze"
+} else if (xpHeroi >= 2001 && xpHeroi < 5000) {
+  nivel = "Prata"
+} else if (xpHeroi >= 5001 && xpHeroi < 7000) {
+  nivel = "Ouro"
+} else if (xpHeroi >= 7001 && xpHeroi < 8000) {
+  nivel = "Platina"
+} else if (xpHeroi >= 8001 && xpHeroi < 9000) {
+  nivel = "Ascendente"
+} else if (xpHeroi >= 9001 && xpHeroi < 10000) {
+  nivel = "Imortal"
+} else {
+  nivel = "Radiante"
+}
+
+// SAÍDA
+console.log(
+  `O herói ${nomeHeroi}, com ${xpHeroi} pontos de experiência, está no nível ${nivel}.`
+)
